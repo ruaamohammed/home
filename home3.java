6b98e6ebd7987addeda1d5882953eb5454fcc686
ruaamohammed <78503310+ruaamohammed@users.noreply.github.com>
2021-02-10 22:00:08 +0300

    Add files via upload

---
+++ home3.java
@@ -0,0 +1,38 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+/**
+ * Created by فونكس تكنولوجي on 10/02/2021.
+ */
+public class home3 {
+    public static int[] removeTheElement(int[] arr,
+                                         int index)
+    {
+        if (arr == null
+                || index < 0
+                || index >= arr.length) {
+            return arr;
+        }
+        int[] anotherArray = new int[arr.length - 1];
+        for (int i = 0, k = 0; i < arr.length; i++) {
+            if (i == index) {
+                continue;
+            }
+            anotherArray[k++] = arr[i];
+        }
+        return anotherArray;
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = { 11, 12, 33, 14, 55 };
+        System.out.println("Original Array: "
+                + Arrays.toString(arr));
+        int index = 2;
+        System.out.println("Index to be removed: "
+                + index);
+        arr = removeTheElement(arr, index);
+        System.out.println("Resultant Array: "
+                + Arrays.toString(arr));
+    }
+}
+
